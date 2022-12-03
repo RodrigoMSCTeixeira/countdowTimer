3b431aea325c2f985f4d38a59428304f46c4c8a6
Rodrigo Teixeira <88559255+RodrigoMSCTeixeira@users.noreply.github.com>
2022-12-03 20:38:24 -0300

    innitial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const countDate = new Date("jan 1, 2023 00:00:00").getTime();
+
+newYear = () => {
+  const now = new Date().getTime();
+  gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const d = Math.floor(gap / day);
+  const h = Math.floor((gap % day) / hour);
+  const m = Math.floor((gap % hour) / minute);
+  const s = Math.floor((gap % minute) / second);
+
+  document.getElementById("days").innerText = d;
+  document.getElementById("hours").innerText = h;
+  document.getElementById("minutes").innerText = m;
+  document.getElementById("seconds").innerText = s;
+};
+
+setInterval(newYear, 1000);
